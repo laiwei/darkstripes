3d892cb322ca9896802e7e87fbd38d0a5505bb26
mauricio lopez <mlopez@dixian.info>
2012-09-16 17:19:59 -0500

    media query nav test

--- searchform.php
+++ searchform.php
@@ -2,4 +2,5 @@
     <fieldset role="search">
         <input class="search" value="<?php echo get_search_query() ?>" results="0" name="s" id="s" placeholder="Search" />
     </fieldset>
+    <fieldset class="mobile-nav"><select><option value="">Navigate…</option><option value="http://melandri.net/">» Home</option><option value="http://melandri.net/about">» About</option><option value="http://melandri.net/projects">» Projects</option><option value="http://melandri.net/archives">» Archives</option><option value="http://photo.melandri.net/">» Photography</option><option value="http://melandri.net/contact">» Contact</option><option value="http://melandri.net/atom.xml">» RSS</option></select></fieldset>
 </form>
