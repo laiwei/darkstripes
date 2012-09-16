328280242635ce2c90084a1472fb4784b45f10a5
mauricio lopez <mlopez@dixian.info>
2012-09-16 16:39:51 -0500

    added support for 404 error.

---
+++ 404.php
@@ -0,0 +1,17 @@
+
+<?php get_header(); ?>
+
+<div id="main">
+<div id="content">
+    <div class="blog-index">
+        <article>
+            <header>
+                <h2 class="entry-title"><?php _e('Error 404. Page Not Found.'); ?></h2>
+            </header> 
+        </article>
+    </div>
+    <?php get_sidebar(); ?>
+</div>
+</div>
+
+<?php get_footer(); ?>
