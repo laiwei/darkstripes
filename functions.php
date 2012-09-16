c421af453b84ed740c5c13088e0244e43568348b
mauricio lopez <mlopez@dixian.info>
2012-09-15 23:19:24 -0500

    changes on functions

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@ define( 'IMAGES', TEMPPATH. "/images");
 
 // menu support
 add_theme_support( 'nav-menus' );
-
 if ( function_exists( 'register_nav_menus' ) ) {
     register_nav_menus(
         array(
@@ -14,6 +13,19 @@ if ( function_exists( 'register_nav_menus' ) ) {
         )
     );
 }
+if (function_exists('wp_page_menu_args')) {
+    function wp_page_menu_args() 
+    {
+        $args['show_home'] = true;
+        return $args;
+    }
+}
+
+// Read More
+function add_p_tag($link){
+    return '<a href=" '. get_permalink() .'#more-'.get_the_ID().'" class="more-link" rel="full-article">Read More →</a>';
+}
+add_filter('the_content_more_link', 'add_p_tag');
 
 // sidebar
 if ( function_exists( 'register_sidebar' ) ) {
