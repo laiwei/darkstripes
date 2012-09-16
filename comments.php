c103e786c472d22bdaabc396dcbf117b9ac4310f
mauricio lopez <mlopez@dixian.info>
2012-09-16 12:09:44 -0500

    fixes on comments loop template

--- comments.php
+++ comments.php
@@ -8,45 +8,42 @@
 ?>
 
     <?php if ( have_comments() ) : ?>
+
+    <div id="comments-list" class="comments">
     
-    <h3 id="comments-title">
-        <?php
+        <h3 id="comments-title">
+            <?php
             printf( 
                 _n( 'One Comment', '%1$s Comments', get_comments_number()),
                 number_format_i18n( get_comments_number() )
             );
-        ?>
-    </h3>
+            ?>
+        </h3>
     
-    <p class="write-comment-link"><a href="#respond"><?php _e( 'Leave a reply &rarr;'); ?></a></p>
+        <p class="write-comment-link"><a href="#respond"><?php _e( 'Leave a reply &rarr;'); ?></a></p>
         
-    <ol class="commentlist">
-        <?php wp_list_comments( ); ?>
-    </ol>
+        <ol>
+            <?php wp_list_comments( ); ?>
+        </ol>
 
-    <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
         <nav id="comment-nav-below">
             <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'yoko' ) ); ?></div>
             <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'yoko' ) ); ?></div>
         </nav>
-    <?php endif;?>
+        <?php endif;?>
+
+    </div>
         
     <?php else: ?>
     
-        <?php if ( comments_open() ) : // If comments are open, but there are no comments ?>
+        <?php if ( comments_open() ) : ?>
             <p class="nocomments"><?php _e( 'No comments yet.'); ?></p>
         <?php else :?>
-            <p class="nocomments"><?php _e( 'Comments are closed.', 'yoko' ); ?></p>
+            <p class="nocomments"><?php _e( 'Comments are closed.'); ?></p>
         <?php endif; ?>
         
     <?php endif; ?>
 
-<?php comment_form(
-array(
-    'comment_notes_before' =>__( '<p class="comment-notes">Required fields are marked <span class="required">*</span>.</p>', 'yoko'),
-    'comment_notes_after' => '',
-    'comment_field'  => '<p class="comment-form-comment"><label for="comment">' . _x( 'Message <span class="required">*</span>', 'noun', 'yoko' ) .     '</label><br/><textarea id="comment" name="comment" rows="8"></textarea></p>',
-)
-); ?>
 
 </div>
