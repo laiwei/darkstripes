8242b09a27a6f2c4b89e614883bd8ad2007f0ed4
mauricio lopez <mlopez@dixian.info>
2012-09-16 12:28:25 -0500

    templates changes on comments form

--- comments.php
+++ comments.php
@@ -23,10 +23,10 @@
         <p class="write-comment-link"><a href="#respond"><?php _e( 'Leave a reply &rarr;'); ?></a></p>
         
         <ol>
-            <?php wp_list_comments( ); ?>
+            <?php wp_list_comments(); ?>
         </ol>
 
-        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
         <nav id="comment-nav-below">
             <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'yoko' ) ); ?></div>
             <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'yoko' ) ); ?></div>
@@ -38,13 +38,19 @@
     <?php else: ?>
     
         <?php if ( comments_open() ) : ?>
-            <p class="nocomments"><?php _e( 'No comments yet.'); ?></p>
+            <?php //add your message ?>
         <?php else :?>
             <p class="nocomments"><?php _e( 'Comments are closed.'); ?></p>
         <?php endif; ?>
         
     <?php endif; ?>
 
-    <?php comment_form(); ?>
+    <?php comment_form(
+    array(
+        'comment_notes_before' =>__( ''),
+        'comment_notes_after' => '',
+        'comment_field'  => '<textarea id="comment" name="comment" rows="8"></textarea>',
+    )   
+    ); ?>
 
 </div>
